d8b10e16b733af688a2229d48749f8d0c80265b5
Damon Heath <damon.t.heath@gmail.com>
2020-04-24 10:19:05 -0500

    add tests and ability to say hello to any input

--- tests.js
+++ tests.js
@@ -18,4 +18,16 @@ describe('sayHello', function () {
     it('should be a defined function', function () {
         expect(typeof sayHello).toBe('function');
     });
+    it('should return a string when called', function () {
+        expect(typeof sayHello()).toBe('string');
+    });
+    it('should return Hello Jane when passed Jane', function () {
+        expect(sayHello("Jane")).toBe("Hello, Jane!");
+    });
+    it('should should return "Hello, Alex!" when passed Alex', function () {
+        expect(sayHello("Alex")).toBe("Hello, Alex!");
+    });
+    it('should should return "Hello, Pat!" when passed Pat', function () {
+        expect(sayHello("Pat")).toBe("Hello, Pat!");
+    });
 });
