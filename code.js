9dd67a5ce0f709fe29bec8653c6f087d37d78e12
Damon Heath <damon.t.heath@gmail.com>
2020-04-28 08:59:33 -0500

    asdf

--- code.js
+++ code.js
@@ -4,5 +4,30 @@ function helloWorld() {
 }
 
 function sayHello(input) {
-    return "Hello, " + input + "!";
+    if (typeof input == "string")
+        return "Hello, " + input + "!";
+    else
+        return "Hello, World!";
+}
+
+function isFive(input) {
+    if (parseFloat(input) === 5)
+        return true;
+    else
+        return false;
+}
+
+function isEven(input) {
+    if (input % 2 === 0)
+        return true;
+    else
+        return false;
+}
+
+function isVowel(input) {
+    return (/^[aeiouy]$/i).test(input)
+}
+
+function add() {
+
 }
--- tests.js
+++ tests.js
@@ -30,4 +30,100 @@ describe('sayHello', function () {
     it('should should return "Hello, Pat!" when passed Pat', function () {
         expect(sayHello("Pat")).toBe("Hello, Pat!");
     });
+    it('should return "Hello, World!" when passed undefined', function () {
+        expect(sayHello()).toBe("Hello, World!");
+    });
+    it('should return "Hello, World!" when passed true', function () {
+        expect(sayHello(true)).toBe("Hello, World!");
+    });
+    it('should return "Hello, World!" when passed false', function () {
+        expect(sayHello(false)).toBe("Hello, World!");
+    });
+});
+
+describe("isFive", function () {
+    it('should be a defined function', function () {
+        expect(typeof isFive).toBe('function');
+    });
+    it('should return a boolean when called', function () {
+        expect(typeof isFive()).toBe('boolean');
+    });
+    it('should return true when passed 5', function () {
+        expect(isFive(5)).toBe(true);
+    });
+    it('should return true when passed "5"', function () {
+        expect(isFive("5")).toBe(true);
+    });
+});
+
+describe("isEven", function () {
+    it('should be a defined function', function () {
+        expect(typeof isEven).toBe('function');
+    });
+    it('should return a boolean when called', function () {
+        expect(typeof isEven()).toBe('boolean');
+    });
+    it('should return true when passed 2', function () {
+        expect(isEven(2)).toBe(true);
+    });
+    it('should return true when passed -4', function () {
+        expect(isEven(-4)).toBe(true);
+    });
+    it('should return true when passed 8', function () {
+        expect(isEven(8)).toBe(true);
+    });
+    it('should return false when passed 3', function () {
+        expect(isEven(3)).toBe(false);
+    });
+    it('should return false when passed "banana"', function () {
+        expect(isEven("banana")).toBe(false);
+    });
+    it('should return false when passed Infinity', function () {
+        expect(isEven(Infinity)).toBe(false);
+    });
+    it('should return false when passed true', function () {
+        expect(isEven(true)).toBe(false);
+    });
+    it('should return false when passed no argument', function () {
+        expect(isEven()).toBe(false);
+    });
+});
+
+describe('isVowel', function () {
+    it('should be a defined function', function () {
+        expect(typeof isVowel).toBe('function');
+    });
+    it('should return a boolean when called', function () {
+        expect(typeof isVowel()).toBe('boolean');
+    });
+    it('should return true when passed a', function () {
+        expect(isVowel("a")).toBe(true);
+    });
+    it('should return true when passed A', function () {
+        expect(isVowel("A")).toBe(true);
+    });
+    it('should return true when passed y', function () {
+        expect(isVowel("Y")).toBe(true);
+    });
+    it('should return false when passed 4', function () {
+        expect(isVowel(4)).toBe(false);
+    });
+    it('should return false when passed true', function () {
+        expect(isVowel(true)).toBe(false);
+    });
+});
+
+describe('add', function () {
+    it('should be a defined function', function () {
+        expect(typeof add).toBe('function');
+    });
+    it('should return 5 when given 2, 3', function () {
+        expect(add(2, 3)).toBe(5);
+    });
+    it('should return -12 when given -3, -9', function () {
+        expect(add(-3, -9)).toBe(-12);
+    });
+    it('should return 11 when given "5", 6', function () {
+        expect(add("5", 6)).toBe(11);
+    });
 });
